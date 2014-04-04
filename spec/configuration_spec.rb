ec20633df251abc0421246cca93312e8a7b0a4a3
Pete Nicholls <pete@metanation.com>
2014-04-04 21:40:19 +1300

    Remove redundant specs

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -4,22 +4,6 @@ require 'spec_helper'
 module Almanac
   describe Configuration do
     
-    describe "#add_ical_feed" do
-      it "accepts a URL" do
-        config = Configuration.new
-        config.add_ical_feed "http://example.org/feed.ics"
-      end
-    end
-    
-    describe "#add_events" do
-      it "accepts an array" do
-        config = Configuration.new
-        config.add_events [
-          { title: "Soul Cake Tuesday" }
-        ]
-      end
-    end
-
     describe "#events" do
       describe "with simple events" do
         it "returns the events" do
