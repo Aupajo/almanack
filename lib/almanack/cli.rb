b33851f8baff545ce7d70e613194a041796430c7
Pete Nicholls <pete@metanation.com>
2014-07-03 22:36:58 +1200

    Add blank project template

--- lib/almanack/cli.rb
+++ lib/almanack/cli.rb
@@ -10,5 +10,31 @@ module Almanack
       say "Almanack version #{VERSION} (#{CODENAME})"
     end
 
+    desc "start", "Start an Almanack server"
+    option :config, aliases: "-c", desc: "Path to config file"
+    def start
+      exec "bundle exec rackup #{options[:config]}"
+    end
+
+    desc "new PATH", "Create a new Almanack project"
+    def new(path)
+      path = Pathname(path).cleanpath
+      directory "new", path
+
+      inside path do
+        say_status :run, "bundle install"
+        system "bundle install --quiet"
+        say
+        say "==> Run your new calendar!"
+        say
+        say "  cd #{path}"
+        say "  almnack start"
+        say
+      end
+    end
+
+    def self.source_root
+      Pathname(__dir__).parent.parent.join("templates")
+    end
   end
 end
---
+++ templates/new/config.ru
@@ -0,0 +1,23 @@
+require 'almanack/server'
+
+Almanack.config do |c|
+  c.title = "My Calendar"
+
+  # Your group's URL name is what you'd find at www.meetup.com/Your-Group-URL-Name/
+  # You can get a Meetup API key from https://secure.meetup.com/meetup_api/key
+  # c.add_meetup_group group_urlname: 'Christchurch-Ruby-Group', key: 'mysecretkey'
+
+  # For a Google Calendar, find the "iCal" link under your Calendar's settings
+  # c.add_ical_feed 'https://example.com/basic.ics'
+
+  # Static events
+  c.add_events [
+    {
+      title: "Edit my calendar's settings",
+      description: "Edit the configuration at #{__FILE__}",
+      start_date: DateTime.now
+    }
+  ]
+end
+
+run Almanack::Server
