aabbab0056ba5602e554a67afc11ca2021ed8c23
Pete Nicholls <pete@metanation.com>
2014-07-06 17:55:45 +1200

    Subscribe link

--- lib/almanack/server.rb
+++ lib/almanack/server.rb
@@ -6,13 +6,20 @@ module Almanack
   class Server < Sinatra::Base
     set :root, -> { Almanack.config.theme_root }
     set :protection, except: :frame_options
+    set :feed_path, "feed.ics"
+
+    helpers do
+      def feed_url
+        "webcal://#{request.host}:#{request.port}/#{settings.feed_path}"
+      end
+    end
 
     get "/" do
       @calendar = Almanack.calendar
       erb :events
     end
 
-    get "/feed.ics" do
+    get "/#{settings.feed_path}" do
       content_type "text/calendar"
       Almanack.calendar.ical_feed
     end
