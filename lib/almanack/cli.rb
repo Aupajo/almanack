03037ec354f7000d3c42b778ba6f82ac17744131
Pete Nicholls <pete@metanation.com>
2014-07-19 12:02:26 +1200

    Add --theme option for generator

--- lib/almanack/cli.rb
+++ lib/almanack/cli.rb
@@ -3,7 +3,18 @@ require "thor"
 module Almanack
   class CLI < Thor
     include Thor::Actions
+
+    SKIP_THEMES = %w( starter )
+
     map "--version" => :version
+
+    def self.available_themes
+      dirs = Pathname(__dir__).join("themes").children.select(&:directory?)
+
+      dirs.map do |path|
+        path.basename.to_s unless SKIP_THEMES.include?(path.basename.to_s)
+      end.compact
+    end
 
     desc "version", "Show Almanack version"
     def version
@@ -17,8 +28,10 @@ module Almanack
     end
 
     desc "new PATH", "Create a new Almanack project"
+    option :theme, default: 'legacy', desc: "Which theme to use (available: #{available_themes.join(', ')})"
     def new(path)
       path = Pathname(path).cleanpath
+      
       directory "new", path
 
       inside path do
@@ -30,6 +43,16 @@ module Almanack
         say "  cd #{path}"
         say "  almnack start"
         say
+      end
+    end
+
+    no_tasks do
+      def theme
+        options[:theme]
+      end
+
+      def available_themes
+        self.class.available_themes
       end
     end
 
