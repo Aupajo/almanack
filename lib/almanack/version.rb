7736a80099dd1e362aefd1b98c421c14ec56d675
Pete Nicholls <pete@metanation.com>
2014-07-27 16:15:34 +1200

    Version 1.0.2

--- lib/almanack/version.rb
+++ lib/almanack/version.rb
@@ -1,6 +1,6 @@
 module Almanack
   CODENAME = "Garlick"
-  VERSION  = "1.0.1"
+  VERSION  = "1.0.2"
   HOMEPAGE = "https://github.com/Aupajo/sinatra-gcal"
   ISSUES   = "https://github.com/Aupajo/sinatra-gcal/issues"
 end
