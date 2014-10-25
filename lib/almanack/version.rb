9ab11a566265f4c544e1cc6d026c3d7424fdb69e
Pete Nicholls <pete@metanation.com>
2014-10-25 22:44:30 +1300

    Update homepage and issues URLs

--- lib/almanack/version.rb
+++ lib/almanack/version.rb
@@ -1,6 +1,6 @@
 module Almanack
   CODENAME = "Garlick"
-  VERSION  = "1.0.4"
-  HOMEPAGE = "https://github.com/Aupajo/sinatra-gcal"
-  ISSUES   = "https://github.com/Aupajo/sinatra-gcal/issues"
+  VERSION  = "1.0.5"
+  HOMEPAGE = "https://github.com/Aupajo/almanack"
+  ISSUES   = "https://github.com/Aupajo/almanack/issues"
 end
