a2a78462982f6fbd1eb59567dc449a790b5824b2
Pete Nicholls <pete@metanation.com>
2014-11-08 12:20:24 +1300

    Extract Representation::IcalFeed

--- lib/almanack/calendar.rb
+++ lib/almanack/calendar.rb
@@ -26,27 +26,7 @@ module Almanack
     end
 
     def ical_feed
-      now = Time.now
-      future = now + ONE_YEAR
-
-      # Three hours is the duration for events missing end dates, a
-      # recommendation suggested by Meetup.com.
-      three_hours = 3 * ONE_HOUR
-
-      ical = RiCal.Calendar
-
-      events_between(now..future).each do |event|
-        ical_event = RiCal.Event
-        ical_event.summary = event.title
-        ical_event.dtstart = event.start_date.utc
-        ical_event.dtend = (event.end_date || event.start_date + three_hours).utc
-        ical_event.description = event.description if event.description
-        ical_event.location = event.location if event.location
-
-        ical.add_subcomponent(ical_event)
-      end
-
-      ical.to_s
+      Representation::IcalFeed.from(self)
     end
   end
 end
---
+++ lib/almanack/representation/ical_feed.rb
@@ -0,0 +1,54 @@
+module Almanack
+  module Representation
+    class IcalFeed
+      attr_reader :calendar
+
+      def initialize(calendar)
+        @calendar = calendar
+      end
+
+      def ical
+        @ical ||= begin
+          ical_component = RiCal.Calendar
+
+          calendar.events_between(now..max_time).each do |event|
+            ical_event = RiCal.Event
+            ical_event.summary = event.title
+            ical_event.dtstart = event.start_date.utc
+            ical_event.dtend = (event.end_date || event.start_date + default_event_duration ).utc
+            ical_event.description = event.description if event.description
+            ical_event.location = event.location if event.location
+
+            ical_component.add_subcomponent(ical_event)
+          end
+
+          ical_component
+        end
+      end
+
+      def to_s
+        ical.to_s
+      end
+
+      def self.from(calendar)
+        self.new(calendar).to_s
+      end
+
+      private
+
+      def max_time
+        now + ONE_YEAR
+      end
+
+      def default_event_duration
+        # Three hours is the duration for events missing end dates, a
+        # recommendation suggested by Meetup.com.
+        3 * ONE_HOUR
+      end
+
+      def now
+        @now ||= Time.now
+      end
+    end
+  end
+end
