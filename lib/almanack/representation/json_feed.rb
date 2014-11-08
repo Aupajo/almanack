29bfcd82a5363870367ecb3914c477552be5f884
Pete Nicholls <pete@metanation.com>
2014-11-08 18:06:33 +1300

    Delegate JSONFeed.from to JSONFeed#to_s

--- lib/almanack/representation/json_feed.rb
+++ lib/almanack/representation/json_feed.rb
@@ -1,8 +1,24 @@
 module Almanack
   module Representation
     class JSONFeed
+      attr_reader :calendar
+
+      def initialize(calendar)
+        @calendar = calendar
+      end
+
+      def to_s
+        to_hash.to_json
+      end
+
       def self.from(calendar)
-        {}.to_json
+        self.new(calendar).to_s
+      end
+
+      private
+
+      def to_hash
+        {}
       end
     end
   end
