61229a000499750199d814790c3c7b73e284775b
Pete Nicholls <pete@metanation.com>
2014-12-10 20:35:18 +1300

    Version 1.1.0.beta2

--- lib/almanack/version.rb
+++ lib/almanack/version.rb
@@ -1,6 +1,6 @@
 module Almanack
   CODENAME = "Garlick"
-  VERSION  = "1.1.0.beta1"
+  VERSION  = "1.1.0.beta2"
   HOMEPAGE = "https://github.com/Aupajo/almanack"
   ISSUES   = "https://github.com/Aupajo/almanack/issues"
 end
