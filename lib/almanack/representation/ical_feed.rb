6d5aa92ef6aabb646a75924644c78b5a91f9a24a
Phitherek_ <phitherek@gmail.com>
2017-11-08 22:05:59 +0100

    Fix different handling of Time and DateTime in ICS

--- lib/almanack/representation/ical_feed.rb
+++ lib/almanack/representation/ical_feed.rb
@@ -34,8 +34,16 @@ module Almanack
       def ical_event_for(event)
         ical_event = RiCal.Event
         ical_event.summary = event.title
-        ical_event.dtstart = event.start_time.new_offset('+00:00')
-        ical_event.dtend = (event.end_time || event.start_time + default_event_duration ).new_offset('+00:00')
+        if event.start_time.is_a?(DateTime)
+          ical_event.dtstart = event.start_time.new_offset('+00:00')
+        elsif event.start_time.is_a?(Time)
+          ical_event.dtstart = event.start_time.utc
+        end
+        if (event.end_time.present? && event.end_time.is_a?(DateTime)) || event.start_time.is_a?(DateTime)
+          ical_event.dtend = (event.end_time || event.start_time + default_event_duration ).new_offset('+00:00')
+        elsif (event.end_time.present? && event.end_time.is_a?(Time)) || event.start_time.is_a?(Time)
+          ical_event.dtend = (event.end_time || event.start_time + default_event_duration ).utc
+        end
         ical_event.description = event.description if event.description
         ical_event.location = event.location if event.location
         ical_event
