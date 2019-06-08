5b66807d95a11fa2beb9dba3c50f86014df1ba23
Pete Nicholls <aupajo@gmail.com>
2019-06-09 09:16:11 +1200

    Version 1.1.3.rc1
    Addresses handling dates in iCal feeds.

--- lib/almanack/version.rb
+++ lib/almanack/version.rb
@@ -1,6 +1,6 @@
 module Almanack
   CODENAME = "Garlick"
-  VERSION  = "1.1.2"
+  VERSION  = "1.1.3.rc1"
   HOMEPAGE = "https://github.com/Aupajo/almanack"
   ISSUES   = "https://github.com/Aupajo/almanack/issues"
 end
