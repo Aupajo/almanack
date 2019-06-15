c3cf164b74827ed94d2caf6565d5b92c8947dd10
Pete Nicholls <aupajo@gmail.com>
2019-06-15 13:33:19 +1200

    Version 1.1.3
    Support full day events.

--- lib/almanack/version.rb
+++ lib/almanack/version.rb
@@ -1,6 +1,6 @@
 module Almanack
   CODENAME = "Garlick"
-  VERSION  = "1.1.3.rc1"
+  VERSION  = "1.1.3"
   HOMEPAGE = "https://github.com/Aupajo/almanack"
   ISSUES   = "https://github.com/Aupajo/almanack/issues"
 end
